15e1e9d0c7a3f4c4fec7f2f0f60b42cb0932ca1a
rahatechnologies <117338820+rahatechnologies@users.noreply.github.com>
2022-12-04 18:23:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// You can code JavaScript here
+
+function speak() {
+    var voice = new SpeechSynthesisUtterance();
+    voice.text = "This is the shoreline at Shanghai, China";
+    speechSynthesis.speak(voice);
+}
